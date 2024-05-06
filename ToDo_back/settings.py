fd5f5f8eea7999ac1a4b4c10fbefe2847900cb0d
arklem <jrsy.tdh@yandex.ru>
2024-05-06 20:54:34 +0300

    update settings

--- ToDo_back/settings.py
+++ ToDo_back/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from .secret_settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -8,7 +9,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*#+m^ck2sx9=*=-b(wuwdnouzw(e=4x4w^-)x(t3^26@ikw)*s'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
